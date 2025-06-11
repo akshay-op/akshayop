22cf27582bcb26370b8d29650351495753285ed5
akshay-op <akshayop277@gmail.com>
2025-06-11 11:15:53 +0530

    after repo rename

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // base: "/akshayop.github.io",
-  base :"/",
+  base :"/akshayop/",
   // server: {
   //   host: "::",
   //   port: 8080,
